d82a70f6600fc424a788688e0be4c111e4bb0837
Luther-we <luther.we@gmail.com>
2018-11-29 14:18:13 +0100

    test send mail

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ io.on("connection", socket => {
             text: 'and easy to do anywhere, even with Node.js',
             html: '<strong>and easy to do anywhere, even with Node.js</strong>',
         };
-        // sgMail.send(msg);
+        sgMail.send(msg);
     })
 
     socket.on("disconnect", () => console.log("Client disconnected"));
